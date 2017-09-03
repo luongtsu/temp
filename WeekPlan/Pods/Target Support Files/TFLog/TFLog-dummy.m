07b004831397788001737c7580e63f89e439195e
Hiep Luong Minh <hluongminh@pentalog.fr>
2017-09-03 11:34:20 +0700

    Init project 03/09/2017

--- WeekPlan/Pods/Target Support Files/TFLog/TFLog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TFLog : NSObject
-@end
-@implementation PodsDummy_TFLog
-@end
--- WeekPlan/WeekPlan/Features/Trend/TrendMain/TrendMainVM.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TrendMainVM.swift
-//  WeekPlan
-//
-//  Created by Luong Minh Hiep on 8/19/17.
-//  Copyright © 2017 Luong Minh Hiep. All rights reserved.
-//
-
-import Foundation
